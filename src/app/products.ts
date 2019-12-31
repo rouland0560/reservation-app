8c002d41dd3e24ea4d6c67a4e56eec44ba43cc4e
Goro <rouland_0217@yahoo.co.jp>
2020-01-01 02:01:52 +0900

    Angularフロント側作成

---
+++ src/app/products.ts
@@ -0,0 +1,57 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'A',
+    heading2: 'B',
+    heading3: 'C',
+    headingText1: '123',
+    headingText2: '456',
+    headingText3: '789',
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'D',
+    heading2: 'E',
+    heading3: 'F',
+    headingText1: '123',
+    headingText2: '456',
+    headingText3: '789',
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1: 'G',
+    heading2: 'H',
+    heading3: 'I',
+    headingText1: '123',
+    headingText2: '456',
+    headingText3: '789',
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone nano',
+    price: 599,
+    description: 'nano size',
+    heading1: 'J',
+    heading2: 'K',
+    heading3: 'L',
+    headingText1: '123',
+    headingText2: '456',
+    headingText3: '789',
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
